17f8fc14820c802d798bbe153e69c87e17bec42e
Igor Korolev <in.earth@ya.ru>
2020-04-19 19:20:27 +0300

    Fix renaming script - uncomment necessary lines

--- rename.sh
+++ rename.sh
@@ -10,10 +10,9 @@ NEW_PROJECT_NAME=$1
 CYAN='\033[0;36m'
 NOCOLOR='\033[0m'
 
-printf "%bRenaming project from %s to %s...%b\n" "${CYAN}" "$OLD_PROJECT_NAME"
-"$NEW_PROJECT_NAME" "${NOCOLOR}"
-#mv "$OLD_PROJECT_NAME" "$NEW_PROJECT_NAME"
-#find . -type f -not -path "./.*" | xargs perl -p -i -e "s/$OLD_PROJECT_NAME/$NEW_PROJECT_NAME/g"
+printf "%bRenaming project from %s to %s...%b\n" "${CYAN}" "$OLD_PROJECT_NAME" "$NEW_PROJECT_NAME" "${NOCOLOR}"
+mv "$OLD_PROJECT_NAME" "$NEW_PROJECT_NAME"
+find . -type f -not -path "./.*" | xargs perl -p -i -e "s/$OLD_PROJECT_NAME/$NEW_PROJECT_NAME/g"
 
 
 printf "%bRun simple test...%b\n" "${CYAN}" "${NOCOLOR}"
